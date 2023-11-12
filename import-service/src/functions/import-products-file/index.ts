278c811329dceb9a463ee737d54e14b8b66b0ad1
neblocker <114916761+neblocker@users.noreply.github.com>
2023-11-12 15:42:25 +0500

    middy: cors enabled
    Co-authored-by: Jamshid Tursunov <zjamshidt@pttep.com>

--- import-service/src/functions/import-products-file/index.ts
+++ import-service/src/functions/import-products-file/index.ts
@@ -6,7 +6,8 @@ export default {
     {
       http: {
         method: "get",
-        path: "import"
+        path: "import",
+        cors: true
       }
     }
   ]
--- product-service/src/functions/product-list/handler.ts
+++ product-service/src/functions/product-list/handler.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyResult } from "aws-lambda"
 import { getProductsList } from "../../services/getProducts"
+import { middyfy } from "@libs/lambda"
 
-export const getProducts = async (): Promise<APIGatewayProxyResult> => {
+export const handler = async (): Promise<APIGatewayProxyResult> => {
   try {
     const products = await getProductsList()
 
@@ -9,21 +10,16 @@ export const getProducts = async (): Promise<APIGatewayProxyResult> => {
 
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true
-      },
       body: JSON.stringify(products)
     }
   } catch (error) {
     return {
       statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
       body: JSON.stringify({
         error: "Internal Server Error."
       })
     }
   }
 }
+
+export const getProducts = middyfy(handler)
